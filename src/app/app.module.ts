cb38ffbcd0793f8f5324ac92aad981d5aa141197
zookeroro <zookeroro@gmail.com>
2020-06-08 21:11:10 +0700

    ๊๊Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,17 @@ import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { ConcludePageComponent } from './page/conclude-page/conclude-page.component';
 import { QuestionnairePageComponent } from './page/questionnaire-page/questionnaire-page.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-
+    NavbarComponent, ConcludePageComponent, QuestionnairePageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({}, {})
+    StoreModule.forRoot({}, {}),
   ],
   providers: [],
   bootstrap: [AppComponent]
